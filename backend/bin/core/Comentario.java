f834e748d67694c5a51fcc8d86181cfa9074c832
1569037 <57673130+1569037@users.noreply.github.com>
2022-04-23 12:16:48 +0200

    Update Comentario.java

--- backend/bin/core/Comentario.java
+++ backend/bin/core/Comentario.java
@@ -1,4 +1,43 @@
 package core;
+import netscape.javascript.JSObject;
+
+import java.util.Date;
 
 public class Comentario {
+    private Usuario autor;
+    private Date fecha;
+    private String contenido;
+
+    public Comentario(Usuario autor, Date fecha, String contenido){
+        this.autor = autor;
+        this.fecha = new Date(fecha.getTime());
+        this.contenido = contenido;
+    }
+    private JSObject toJson(){
+        return null;
+    }
+
+    public Usuario getAutor() {
+        return autor;
+    }
+
+    public void setAutor(Usuario autor) {
+        this.autor = autor;
+    }
+
+    public Date getFecha() {
+        return fecha;
+    }
+
+    public void setFecha(Date fecha) {
+        this.fecha = fecha;
+    }
+
+    public String getContenido() {
+        return contenido;
+    }
+
+    public void setContenido(String contenido) {
+        this.contenido = contenido;
+    }
 }
