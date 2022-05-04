cd67f9885a4959361241dc74340cc7f303850467
danielalcover1 <1562889@uab.cat>
2022-05-04 18:05:59 +0200

    cambiado fecha de nacimiento en controller

--- backend/bin/core/UsuarioController.java
+++ backend/bin/core/UsuarioController.java
@@ -3,6 +3,8 @@ package core;
 import org.springframework.web.bind.annotation.*;
 
 import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 import java.util.List;
 import java.util.Map;
 
@@ -11,27 +13,34 @@ public class UsuarioController {
 
   //creacion usuario despues de recibir request y los datos de este
   @PostMapping("/user/create")
-  public Usuario index(@RequestBody Map<String, String> body){
+  public Usuario create_user(@RequestBody Map<String, String> body){
     String name = body.get("name");
     String pwd = body.get("password");
     String foto = body.get("photo");
     String email = body.get("email");
-    return new Usuario(name,pwd, LocalDate.now(),foto,email);
+    String birth_date = body.get("date_birth");
+    DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd-MM-yyyy");
+    LocalDate nacimiento = LocalDate.parse(birth_date, formatter);
+    return new Usuario(name,pwd, nacimiento,foto,email);
   }
  //Ejemplo de internet para ver los diferentes tipos de peticiones y como hacer la respuesta
   @GetMapping("/user/{id}")
   public String search(@PathVariable String id){
     return id;
   }
-/*
-  @PostMapping("/blog")
-  public Blog create(@RequestBody Map<String, String> body){
-    int id = Integer.parseInt(body.get("id"));
-    String title = body.get("title");
-    String content = body.get("content");
-    return blogMockedData.createBlog(id, title, content);
-  }
 
+  @PostMapping("/user/register")
+  public Usuario register_user(@RequestBody Map<String, String> body){
+    String name = body.get("name");
+    String pwd = body.get("password");
+    String foto = body.get("photo");
+    String email = body.get("email");
+    String birth_date = body.get("date_birth");
+    DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd-MM-yyyy");
+    LocalDate nacimiento = LocalDate.parse(birth_date, formatter);
+    return new Usuario(name,pwd, nacimiento,foto,email);
+  }
+/*
   @PutMapping("/blog/{id}")
   public Blog update(@PathVariable String id, @RequestBody Map<String, String> body){
     int blogId = Integer.parseInt(id);
