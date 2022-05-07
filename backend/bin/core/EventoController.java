c3a0955fe8d78693f196869af6fd20a3870df19f
Enrique Gómez <enriquegomezbecerra213@gmail.com>
2022-05-07 09:38:12 +0200

    Cambios unirse a evento con GET

--- backend/bin/core/EventoController.java
+++ backend/bin/core/EventoController.java
@@ -1,9 +1,6 @@
 package core;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.Map;
 
@@ -11,7 +8,7 @@ import java.util.Map;
 public class EventoController {
 
   @GetMapping("/event/join_event/{event_id}&{user_id}")
-  public String join_event(@RequestBody String event_id, String user_id){
+  public String join_event(@PathVariable("event_id") String event_id, @PathVariable("user_id") String user_id){
     Evento event = new Evento(Integer.parseInt(event_id));
     if (event.anadirParticipante(Integer.parseInt(user_id))) {
       return "User joined the event successfully";
