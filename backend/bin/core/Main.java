31391f8cc8b196a5c624b2bd6a19b87cd8f8f213
danielalcover1 <1562889@uab.cat>
2022-05-14 11:41:57 +0200

    eliminado error Main.java que no dejaba ejecutar

--- backend/bin/core/Main.java
+++ backend/bin/core/Main.java
@@ -1,6 +1,7 @@
 package core;
 
 import java.time.LocalDate;
+import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -9,7 +10,7 @@ public class Main {
     //Usuario.registrar("Valorador","algo",LocalDate.now(),"foto","mail@mail.mail");
     //Usuario.registrar("Valorado","algo",LocalDate.now(),"foto","mail3@mail.mail");
     //Usuario.valorarUsuario(81,10,83);
-    Evento.crear("Algo", new Punto(1,1),"Nuevo", LocalDate.now(),new Usuario(81),new ArrayList<String>() );
+    Evento.crear("Algo", new Punto(1,1),"Nuevo", LocalDateTime.now(),new Usuario(81),new ArrayList<String>() );
   }
 
 
