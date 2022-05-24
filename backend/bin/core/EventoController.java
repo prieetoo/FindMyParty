66c2d4c83cdf0d4b25641f072fd3456ad735a79e
AlexFernandezOcon1 <alexocon1@gmail.com>
2022-05-24 19:02:49 +0200

    Json format on EventoController

--- backend/bin/core/EventoController.java
+++ backend/bin/core/EventoController.java
@@ -18,9 +18,9 @@ public class EventoController {
   public String join(@PathVariable("event_id") String event_id, @PathVariable("user_id") String user_id){
     // 1 correct, -1 error
     if (Evento.anadirParticipante(Integer.parseInt(event_id), Integer.parseInt(user_id))) {
-      return "\"result\":1";
+      return "{\"result\":1}";
     }
-    return "\"result\":-1";
+    return "{\"result\":-1}";
   }
 
   @PostMapping(value = "/event/create", consumes = MediaType.APPLICATION_JSON_VALUE,
@@ -36,36 +36,36 @@ public class EventoController {
     ArrayList<String> etiquetas = body.get("tickets");
     // 1 correct, -1 error
     if (!Evento.crear(informacion.get(0), p, ubicacion, fecha, anfitrion, etiquetas)) {
-      return "\"result\":-1";
+      return "{\"result\":1}";
     }
-    return "\"result\":1";
+    return "{\"result\":-1}";
   }
 
   @GetMapping("/event/comment_event/{event_id}/{user_id}/{content}")
   public String comment_event(@PathVariable("event_id") String event_id, @PathVariable("user_id") String user_id, @PathVariable("content") String content){
     // 1 correct, -1 error
     if (Evento.comentar(Integer.parseInt(event_id), Integer.parseInt(user_id), content)) {
-      return "\"result\":1";
+      return "{\"result\":1}";
     }
-    return "\"result\"-1";
+    return "{\"result\":-1}";
   }
 
   @GetMapping(value = "/event/eliminate/{event_id}")
   public String create_event(@PathVariable("event_id") String event_id){
     // 1 correct, -1 error
     if (!Evento.eliminar(Integer.parseInt(event_id))) {
-      return "\"result\":-1";
+      return "{\"result\":1}";
     }
-    return "\"result\":1";
+    return "{\"result\":-1}";
   }
 
   @PostMapping("/event/valorar_event")
   public String valorar_usuario(@RequestBody Map<String, String> body){
     // 1 correct, -1 error
     if (!Evento.valorar(Integer.parseInt(body.get("destinatario")),Float.parseFloat(body.get("valor")), Integer.parseInt(body.get("autor")))) {
-      return "\"result\"-1";
+      return "{\"result\":1}";
     }
-    return "\"result\":1";
+    return "{\"result\":-1}";
   }
 
 }
