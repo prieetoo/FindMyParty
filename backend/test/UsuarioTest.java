ccc98adf3ec6c1d495a35c0b395468cb3e9f065b
danielalcover1 <1562889@uab.cat>
2022-05-29 18:38:37 +0200

    nuevos tests completados

--- backend/test/UsuarioTest.java
+++ backend/test/UsuarioTest.java
@@ -13,23 +13,50 @@ public class UsuarioTest {
   public static String email2;
   public static String nombre2;
   public static String user_id2;
+  public static String password1;
+  public static String password2;
+  public static String email_incorrecto;
+  public static String password_incorrecta;
 
   @BeforeAll
   public static void init(){
     nombre1 = "juanjo";
-    String password1 = "12345";
+    password1 = "12345";
     LocalDate fechaNacimiento1 = LocalDate.now();
     String foto1 = "";
     email1 = "juanjo@uab.cat";
     String aux1 = registrar(nombre1, password1, fechaNacimiento1, foto1, email1);
     user_id1 = aux1.substring(11, 14);
     nombre2 = "Diego";
-    String password2 = "123456";
+    password2 = "123456";
     LocalDate fechaNacimiento2 = LocalDate.now();
     String foto2 = "";
     email2 = "diego@uab.cat";
     String aux2 = registrar(nombre2, password2, fechaNacimiento2, foto2, email2);
     user_id2 = aux2.substring(11, 14);
+    email_incorrecto = "juanjo@e-campus.uab.cat";
+    password_incorrecta = "398567";
+  }
+
+  @Test
+  public void TestLogin1(){ //correo correcto
+    String result = iniciarSesion(email1, password1);
+    System.out.println(result);
+    assert result.equals("{\"result\":\""+ user_id1 +"\"}");
+  }
+
+  @Test
+  public void TestLogin2(){ //password incorrecta
+    String result = iniciarSesion(email1, password_incorrecta);
+    System.out.println(result);
+    assert result.equals("{\"result\":\"-1\"}");
+  }
+
+  @Test
+  public void TestLogin3(){ //correo incorrecto
+    String result = iniciarSesion(email_incorrecto, password1);
+    System.out.println(result);
+    assert result.equals("{\"result\":\"-2\"}");
   }
 
   @Test
@@ -41,8 +68,7 @@ public class UsuarioTest {
 
   @Test
   public void TestRecover_password2(){ //correo incorrecto
-    String email_inc = "juanjo@e-campus.uab.cat";
-    String result = recuperarPassword(email_inc);
+    String result = recuperarPassword(email_incorrecto);
     System.out.println(result);
     assert result.equals("{\"result\":\"-1\"}");
   }
